42b903ec4d5a9f8d59faf6502473bc3d4940b2ca
Runhan Li <rli@Runhans-MacBook-Pro.local>
2021-01-18 00:20:26 -0800

    Check in Ecommerce

---
+++ java/web-application/EcommerceSystem/src/test/java/com/example/demo/TestUtils.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import java.lang.reflect.Field;
+
+public class TestUtils {
+    public static void injectObjects(Object target, String fieldName, Object toInject) {
+        //boolean wasPrivate = false;
+
+        try {
+            Field f = target.getClass().getDeclaredField(fieldName);
+            f.setAccessible(true);
+            f.set(target, toInject);
+        }
+        catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
